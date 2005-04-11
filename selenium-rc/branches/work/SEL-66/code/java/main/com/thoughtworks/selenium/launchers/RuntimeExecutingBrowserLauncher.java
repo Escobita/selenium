b04a0744b60adfe38ea7986b38152acb28fdafff
mdub <mdub@07704840-8298-11de-bf8c-fd130f914ac9>
2005-04-11 11:16:01 +0000

    Experimental branch for re-design of TestRunner UI (SEL-66).
    git-svn-id: http://selenium.googlecode.com/svn@837 07704840-8298-11de-bf8c-fd130f914ac9

---
+++ selenium-rc/branches/work/SEL-66/code/java/main/com/thoughtworks/selenium/launchers/RuntimeExecutingBrowserLauncher.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.selenium.launchers;
+
+import com.thoughtworks.selenium.BrowserLauncher;
+import com.thoughtworks.selenium.SeleniumException;
+
+import java.io.IOException;
+
+/**
+ * @author Paul Hammant
+ * @version $Revision: 1.8 $
+ */
+public abstract class RuntimeExecutingBrowserLauncher implements BrowserLauncher {
+
+    protected Process process;
+    private String commandPath;
+
+    protected RuntimeExecutingBrowserLauncher(String commandPath) {
+        this.commandPath = commandPath;
+    }
+
+    public void launch(String url) {
+        exec(commandPath + " " + url);
+    }
+
+    protected void exec(String command) {
+
+        try {
+            process = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            throw new SeleniumException("IO Exception:" + e.getMessage());
+        }
+    }
+}
