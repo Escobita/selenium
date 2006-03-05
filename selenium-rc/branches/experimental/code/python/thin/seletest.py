a1ffe0d0d5146e7af0b671868b7f060d259a5bfa
dfabulich <dfabulich@07704840-8298-11de-bf8c-fd130f914ac9>
2006-03-05 05:55:24 +0000

    Updated thin Python client to work with the latest server
    git-svn-id: http://selenium.googlecode.com/svn@1749 07704840-8298-11de-bf8c-fd130f914ac9

--- selenium-rc/branches/experimental/code/python/thin/seletest.py
+++ selenium-rc/branches/experimental/code/python/thin/seletest.py
@@ -3,28 +3,24 @@ import seletest
 import selenium
 
 seleniumHost = 'localhost'
-seleniumPort = str(8080)
+seleniumPort = str(4444)
+#browserStartCommand = "c:\\program files\\internet explorer\\iexplore.exe"
+browserStartCommand = "*firefox"
+browserURL = "http://www.irian.at"
 
-def chooseSeleniumServer(host, port):
+def chooseSeleniumServer(host, port, browserCommand, browserURL):
 	seletest_class.seleniumHost = host
 	seletest_class.seleniumPort = str(port)
+	seletest_class.browserStartCommand = browserCommand
+	seletest_class.browserURL = browserURL
 
 
 class seletest_class(unittest.TestCase):
 
-    def setUp(self):
-	print "Using selenium server at " + seletest.seleniumHost + ":" + seletest.seleniumPort
-	self.seleniumField = selenium.selenium_class()
+	def setUp(self):
+		print "Using selenium server at " + seletest.seleniumHost + ":" + seletest.seleniumPort
+		self.seleniumField = selenium.selenium_class(seleniumHost, seleniumPort)
+		self.seleniumField.start(browserStartCommand, browserURL)
 
-    def open(self, url):
- 	print "seletest:open called"
-        self.seleniumField.open(url)
-
-    def type(self, arg1, arg2):
-        self.seleniumField.type(arg1, arg2)
-
-    def verify_text_present(self, s):
-        self.seleniumField.verify_text_present(s)
-
-    def keydown(self, arg1, arg2):
-        self.seleniumField.keydown(arg1, arg2)
+	def tearDown(self):
+		self.seleniumField.stop()
