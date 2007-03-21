e9c2e85cdf77829eac6a4b8c2c27619d2dd3d7bb
shinya <shinya@07704840-8298-11de-bf8c-fd130f914ac9>
2007-03-21 14:41:42 +0000

    release 0.8.7
    git-svn-id: http://selenium.googlecode.com/svn@3855 07704840-8298-11de-bf8c-fd130f914ac9

---
+++ selenium-ide/tags/RELEASE_0_8_7/content/formats/html.js
@@ -0,0 +1,325 @@
+function decodeText(text) {
+	var escapeXml = options.escapeXmlEntities;
+	var r;
+	text = text.replace(/<br\s*\/?>/gi, "\n");
+	if (escapeXml == 'always' || escapeXml == 'partial' || escapeXml == 'html') {
+		text = text.replace(/&lt;/g, '<');
+		text = text.replace(/&gt;/g, '>');
+	}
+	if (escapeXml == 'html') {
+		text = text.replace(/&nbsp;/g, "\xA0");
+		text = text.replace(/&#(\d+);/g, function(str, p1) { 
+								 return String.fromCharCode(parseInt(p1));
+							 });
+		text = text.replace(/&#x([0-9a-f]+);/gi, function(str, p1) { 
+								 return String.fromCharCode(parseInt(p1, 16));
+							 });
+		text = text.replace(/ +/g, " "); // truncate multiple spaces to single space
+		text = text.replace(/\xA0/g, " ");
+	}
+	if (escapeXml == 'always' || escapeXml == 'html') {
+		text = text.replace(/&apos;/g, "'");
+		text = text.replace(/&quot;/g, '"');
+		text = text.replace(/&amp;/g, '&');
+	}
+	if ('true' == options.escapeDollar) {
+		text = text.replace(/([^\\])\$\{/g, '$1$$$${'); // replace ${...} to $${...}
+		text = text.replace(/^\$\{/g, '$$$${'); // replace ${...} to $${...}
+		text = text.replace(/\\\$\{/g, '$${'); // replace \${...} to ${...}
+	}
+	text = text.replace(/^\s+/, "");
+	text = text.replace(/\s+$/, "");
+	return text;
+}
+
+function encodeText(text) {
+	var escapeXml = options.escapeXmlEntities;
+	if (escapeXml == 'always') {
+		// & -> &amp;
+		// &amp; -> &amp;amp;
+		// &quot; -> &amp;quot;
+		// &nbsp; -> &nbsp;
+		text = text.replace(/&(\w+);/g, '%%tmp_entity%%$1%%');
+		text = text.replace(/%%tmp_entity%%(amp|apos|quot|lt|gt)%%/g, '&$1;');
+		text = text.replace(/&/g, '&amp;');
+		text = text.replace(/%%tmp_entity%%(\w+)%%/g, '&$1;');
+		text = text.replace(/\'/g, '&apos;');
+		text = text.replace(/\"/g, '&quot;');
+	} else if (escapeXml == 'html') {
+		// & -> &
+		// ' -> '
+		// \xA0 -> &nbsp;
+		// &amp; -> &amp;amp;
+		// &quot; -> &amp;quot;
+		// &nbsp; -> &amp;nbsp;
+		text = text.replace(/&(nbsp|amp|quot|apos|lt|gt|\d+|x\d+)(;|\W)/g, '&amp;$1$2');
+		text = text.replace(/\xA0/g, '&nbsp;');
+		text = text.replace(/ {2,}/g, function(str) {
+				var result = '';
+				for (var i = 0; i < str.length; i++) {
+					result += '&nbsp;';
+				}
+				return result;
+			}); // convert multiple spaces to nbsp
+	}
+	if (escapeXml == 'always' || escapeXml == 'partial' || escapeXml == 'html') {
+		text = text.replace(/</g, '&lt;');
+		text = text.replace(/>/g, '&gt;');
+	}
+	if ('true' == options.escapeDollar) {
+		text = text.replace(/([^\$])\$\{/g, '$1\\${'); // replace ${...} to \${...}
+		text = text.replace(/^\$\{/g, '\\${'); // replace ${...} to \${...}
+		text = text.replace(/\$\$\{/g, '${'); // replace $${...} to ${...}
+	}
+    text = text.replace(/\n/g, "<br />");
+	return text;
+}
+
+function convertText(command, converter) {
+	var props = ['command', 'target', 'value'];
+	for (var i = 0; i < props.length; i++) {
+		var prop = props[i];
+		command[prop] = converter(command[prop]);
+	}
+}
+
+/**
+ * Parse source and update TestCase. Throw an exception if any error occurs.
+ *
+ * @param testCase TestCase to update
+ * @param source The source to parse
+ */
+function parse(testCase, source) {
+	var commandRegexp = new RegExp(options.commandLoadPattern, 'i');
+	var commentRegexp = new RegExp(options.commentLoadPattern, 'i');
+	var commandOrCommentRegexp = new RegExp("((" + options.commandLoadPattern + ")|(" + options.commentLoadPattern + "))", 'ig');
+	var doc = source;
+	var commands = [];
+	var commandFound = false;
+	var lastIndex;
+	while (true) {
+		//log.debug("doc=" + doc + ", commandRegexp=" + commandRegexp);
+		lastIndex = commandOrCommentRegexp.lastIndex;
+		var docResult = commandOrCommentRegexp.exec(doc);
+		if (docResult) {
+			if (docResult[2]) { // command
+				var command = new Command();
+				command.skip = docResult.index - lastIndex;
+				command.index = docResult.index;
+				var result = commandRegexp.exec(doc.substring(lastIndex));
+				eval(options.commandLoadScript);
+				convertText(command, decodeText);
+				commands.push(command);
+				if (!commandFound) {
+					// remove comments before the first command or comment
+					for (var i = commands.length - 1; i >= 0; i--) {
+						if (commands[i].skip > 0) {
+							commands.splice(0, i);
+							break;
+						}
+					}
+					testCase.header = doc.substr(0, commands[0].index);
+					commandFound = true;
+				}
+			} else { // comment
+				var comment = new Comment();
+				comment.skip = docResult.index - lastIndex;
+				comment.index = docResult.index;
+				var result = commentRegexp.exec(doc.substring(lastIndex));
+				eval(options.commentLoadScript);
+				commands.push(comment);
+			}
+		} else {
+			break;
+		}
+	}
+	if (commands.length > 0) {
+		testCase.footer = doc.substring(lastIndex);
+		log.debug("header=" + testCase.header);
+		log.debug("footer=" + testCase.footer);
+		//log.debug("commands.length=" + commands.length);
+		testCase.commands = commands;
+	} else {
+		throw "no command found";
+	}
+}
+
+function getSourceForCommand(commandObj) {
+	var command = null;
+	var comment = null;
+	var template = '';
+	if (commandObj.type == 'command') {
+		command = commandObj;
+		command = command.createCopy();
+		convertText(command, this.encodeText);
+		template = options.commandTemplate;
+	} else if (commandObj.type == 'comment') {
+		comment = commandObj;
+		template = options.commentTemplate;
+	}
+	var result;
+	var text = template.replace(/\$\{([a-zA-Z0-9_\.]+)\}/g, 
+        function(str, p1, offset, s) {
+            result = eval(p1);
+            return result != null ? result : '';
+        });
+	return text;
+}
+
+/**
+ * Format an array of commands to the snippet of source.
+ * Used to copy the source into the clipboard.
+ *
+ * @param The array of commands to sort.
+ */
+function formatCommands(commands) {
+	var commandsText = '';
+	for (i = 0; i < commands.length; i++) {
+		var text = getSourceForCommand(commands[i]);
+		commandsText = commandsText + text;
+	}
+	return commandsText;
+}
+
+/**
+ * Format TestCase and return the source.
+ * The 3rd and 4th parameters are used only in default HTML format.
+ *
+ * @param testCase TestCase to format
+ * @param name The name of the test case, if any. It may be used to embed title into the source.
+ * @param saveHeaderAndFooter true if the header and footer should be saved into the TestCase.
+ * @param useDefaultHeaderAndFooter Parameter used for only default format.
+ */
+function format(testCase, name, saveHeaderAndFooter, useDefaultHeaderAndFooter) {
+	var text;
+	var commandsText = "";
+	var testText;
+	var i;
+	
+	for (i = 0; i < testCase.commands.length; i++) {
+		var text = getSourceForCommand(testCase.commands[i]);
+		commandsText = commandsText + text;
+	}
+	
+	var testText;
+	if (testCase.header == null || testCase.footer == null || useDefaultHeaderAndFooter) {
+		testText = options.testTemplate;
+		testText = testText.replace(/\$\{name\}/g, name);
+		var encoding = options["global.encoding"];
+		if (!encoding) encoding = "UTF-8";
+		testText = testText.replace(/\$\{encoding\}/g, encoding);
+		var commandsIndex = testText.indexOf("${commands}");
+		if (commandsIndex >= 0) {
+			var header = testText.substr(0, commandsIndex);
+			var footer = testText.substr(commandsIndex + "${commands}".length);
+			testText = header + commandsText + footer;
+			if (saveHeaderAndFooter) {
+				testCase.header = header;
+				testCase.footer = footer;
+			}
+		}
+	} else {
+		testText = testCase.header + commandsText + testCase.footer;
+	}
+	
+	return testText;
+}
+
+/*
+ * Optional: The customizable option that can be used in format/parse functions.
+ */
+this.options = {
+	commandLoadPattern:
+	"<tr\s*[^>]*>" +
+	"\\s*(<!--[\\d\\D]*?-->)?" +
+	"\\s*<td\s*[^>]*>\\s*([\\w]*?)\\s*</td>" +
+	"\\s*<td\s*[^>]*>([\\d\\D]*?)</td>" +
+	"\\s*(<td\s*/>|<td\s*[^>]*>([\\d\\D]*?)</td>)" +
+	"\\s*</tr>\\s*",
+	
+	commandLoadScript:
+	"command.command = result[2];\n" +
+	"command.target = result[3];\n" +
+	"command.value = result[5] || '';\n",
+
+	commentLoadPattern:
+	"<!--([\\d\\D]*?)-->\\s*",
+
+	commentLoadScript:
+	"comment.comment = result[1];\n",
+
+	testTemplate:
+	"<html>\n" +
+	"<head>\n" +
+	'<meta http-equiv="Content-Type" content="text/html; charset=${encoding}">\n' +
+	"<title>${name}</title>\n" +
+	"</head>\n" +
+	"<body>\n" +
+	'<table cellpadding="1" cellspacing="1" border="1">\n'+
+	'<thead>\n' +
+	'<tr><td rowspan="1" colspan="3">${name}</td></tr>\n' +
+	"</thead><tbody>\n" +
+	"${commands}\n" +
+	"</tbody></table>\n" +
+	"</body>\n" +
+	"</html>\n",
+
+	commandTemplate:
+	"<tr>\n" +
+	"\t<td>${command.command}</td>\n" +
+	"\t<td>${command.target}</td>\n" +
+	"\t<td>${command.value}</td>\n" +
+	"</tr>\n",
+
+	commentTemplate:
+	"<!--${comment.comment}-->\n",
+	
+	escapeXmlEntities:
+	"html",
+
+	escapeDollar:
+	"false"
+};
+
+/*
+ * Optional: XUL XML String for the UI of the options dialog
+ */
+this.configForm = 
+	//'<tabbox flex="1"><tabs orient="horizontal"><tab label="Load"/><tab label="Save"/></tabs>' +
+	//'<tabpanels flex="1">' +
+	//'<tabpanel orient="vertical">' +
+	'<description>Regular expression for each command entry</description>' +
+	'<textbox id="options_commandLoadPattern" flex="1"/>' +
+	'<separator class="thin"/>' +
+	'<description>Script to load command from the pattern</description>' +
+	'<textbox id="options_commandLoadScript" multiline="true" flex="1" rows="2"/>' +
+	//'<separator class="thin"/>' +
+	//'<description>Regular expression for comments between commands</description>' +
+	//'<textbox id="options_commentLoadPattern" flex="1"/>' +
+	//'<separator class="thin"/>' +
+	//'<description>Script to load comment from the pattern</description>' +
+	//'<textbox id="options_commentLoadScript" multiline="true" flex="1" rows="2"/>' +
+	'<separator class="groove"/>' +
+	//'</vbox><vbox>' +
+	//'</tabpanel>' +
+	//'<tabpanel orient="vertical">' +
+	'<description>Template for new test html file</description>' +
+	'<textbox id="options_testTemplate" multiline="true" flex="1" rows="3"/>' +
+	'<separator class="thin"/>' +
+	'<description>Template for command entries in the test html file</description>' +
+	'<textbox id="options_commandTemplate" multiline="true" flex="1" rows="3"/>' +
+	'<separator class="groove"/>' +
+	'<hbox align="center"><description>Escape XML / HTML entities?</description>' +
+	'<menulist id="options_escapeXmlEntities">' +
+	'<menupopup>' +
+	'<menuitem label="HTML" value="html"/>' +
+	'<menuitem label="XML - always: &amp; &quot; &apos; &lt; &gt;" value="always"/>' +
+	'<menuitem label="XML - partially: &lt; &gt;" value="partial"/>' +
+	'<menuitem label="never" value="never"/>' +
+	'</menupopup>' +
+	'</menulist></hbox>' +
+	'<checkbox id="options_escapeDollar" label="Escape \'${\' as \'\${\' (useful for JSP 2.0)"/>';
+	//'<separator class="thin"/>' +
+	//'<description>Template for comment entries in the test html file</description>' +
+	//'<textbox id="options_commentTemplate" multiline="true" flex="1"/>' +
+	//'</tabpanel></tabpanels></tabbox>';
---
+++ selenium-ide/tags/RELEASE_0_8_7/content/locatorBuilders.js
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2005 Shinya Kasatani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function LocatorBuilders(window) {
+	this.window = window;
+	this.log = new Log("LocatorBuilders");
+}
+
+LocatorBuilders.prototype.detach = function() {
+	delete this.window._locator_pageBot;
+}
+
+LocatorBuilders.prototype.pageBot = function() {
+	var pageBot = this.window._locator_pageBot;
+	if (pageBot == null) {
+		//pageBot = BrowserBot.createForWindow(this.window);
+        pageBot = new MozillaBrowserBot(this.window);
+        var self = this;
+        pageBot.getCurrentWindow = function() {
+            return self.window;
+        }
+		this.window._locator_pageBot = pageBot;
+	}
+	return pageBot;
+}
+
+LocatorBuilders.prototype.buildWith = function(name, e) {
+	return LocatorBuilders.builderMap[name].call(this, e);
+}
+
+LocatorBuilders.prototype.build = function(e) {
+	var i = 0;
+	var xpathLevel = 0;
+	var maxLevel = 10;
+	var locator;
+	this.log.debug("getLocator for element " + e);
+	
+	for (var i = 0; i < LocatorBuilders.order.length; i++) {
+		var finderName = LocatorBuilders.order[i];
+		this.log.debug("trying " + finderName);
+		locator = this.buildWith(finderName, e);
+		if (locator) {
+			locator = String(locator);
+			this.log.debug("locator=" + locator);
+			// test the locator
+			if (e == this.findElement(locator)) {
+				return locator;
+			}
+		}
+	}
+	return "LOCATOR_DETECTION_FAILED";
+}
+
+LocatorBuilders.prototype.findElement = function(locator) {
+	try {
+		return this.pageBot().findElement(locator);
+	} catch (error) {
+		this.log.debug("findElement failed: " + error + ", locator=" + locator);
+		return null;
+	}
+}
+
+/*
+ * Class methods
+ */
+
+LocatorBuilders.order = [];
+LocatorBuilders.builderMap = {};
+
+LocatorBuilders.add = function(name, finder) {
+	this.order.push(name);
+	this.builderMap[name] = finder;
+}
+
+
+
+/*
+ * Utility function: Encode XPath attribute value.
+ */
+LocatorBuilders.prototype.attributeValue = function(value) {
+	if (value.indexOf("'") < 0) {
+		return "'" + value + "'";
+	} else if (value.indexOf('"') < 0) {
+		return '"' + value + '"';
+	} else {
+		var result = 'concat(';
+		while (true) {
+			var apos = value.indexOf("'");
+			var quot = value.indexOf('"');
+			if (apos < 0) {
+				result += "'" + value + "'";
+				break;
+			} else if (quot < 0) {
+				result += '"' + value + '"';
+				break;
+			} else if (quot < apos) {
+				var part = value.substring(0, apos);
+				result += "'" + part + "'";
+				value = value.substring(part.length);
+			} else {
+				var part = value.substring(0, quot);
+				result += '"' + part + '"';
+				value = value.substring(part.length);
+			}
+			result += ',';
+		}
+		result += ')';
+		return result;
+	}
+}
+
+/*
+ * ===== builders =====
+ */
+
+LocatorBuilders.add('id', function(e) {
+		if (e.id) {
+			return e.id;
+		}
+		return null;
+	});
+
+LocatorBuilders.add('link', function(e) {
+		if (e.nodeName == 'A') {
+			var text = e.textContent;
+			if (!text.match(/^\s*$/)) {
+				return "link=" + exactMatchPattern(text.replace(/\xA0/g, " ").replace(/^\s*(.*?)\s*$/, "$1"));
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('name', function(e) {
+		if (e.name) {
+			return e.name;
+		}
+		return null;
+	});
+
+/*
+ * This function is called from DOM locatorBuilders
+ */
+LocatorBuilders.prototype.findDomFormLocator = function(form) {
+	if (form.hasAttribute('name')) {
+		var name = form.getAttribute('name');
+		var locator = "document." + name;
+		if (this.findElement(locator) == form) {
+			return locator;
+		}
+		locator = "document.forms['" + name + "']";
+		if (this.findElement(locator) == form) {
+			return locator;
+		}
+	}
+	var forms = this.window.document.forms;
+	for (var i = 0; i < forms.length; i++) {
+		if (form == forms[i]) {
+			return "document.forms[" + i + "]";
+		}
+	}
+	return null;
+}
+
+LocatorBuilders.add('domFormElementName', function(e) {
+		if (e.form && e.name) {
+			var formLocator = this.findDomFormLocator(e.form);
+			var candidates = [formLocator + "." + e.name,
+							  formLocator + ".elements['" + e.name + "']"];
+			for (var c = 0; c < candidates.length; c++) {
+				var locator = candidates[c];
+				var found = this.findElement(locator);
+				if (found) {
+					if (found == e) {
+						return locator;
+					} else if (found instanceof NodeList) {
+						// multiple elements with same name
+						for (var i = 0; i < found.length; i++) {
+							if (found[i] == e) {
+								return locator + "[" + i + "]";
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('linkXPath', function(e) {
+		if (e.nodeName == 'A') {
+			var text = e.textContent;
+			if (!text.match(/^\s*$/)) {
+				return "//a[contains(text(),'" + text.replace(/^\s+/,'').replace(/\s+$/,'') + "')]";
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('imgXPath', function(e) {
+        if (e.nodeName == 'IMG') {
+            if (e.alt != '') {
+                return "//img[@alt=" + this.attributeValue(e.alt) + "]";
+            } else if (e.title != '') {
+                return "//img[@title=" + this.attributeValue(e.title) + "]";
+            } else if (e.src != '') {
+				return "//img[contains(@src," + this.attributeValue(e.src) + ")]";
+            }
+        }
+		return null;
+	});
+
+LocatorBuilders.add('attributesXPath', function(e) {
+		const PREFERRED_ATTRIBUTES = ['id','name','value','type','action','onclick'];
+		
+		function attributesXPath(name, attNames, attributes) {
+			var locator = "//" + name + "[";
+			for (var i = 0; i < attNames.length; i++) {
+				if (i > 0) {
+					locator += " and ";
+				}
+				var attName = attNames[i];
+				locator += '@' + attName + "=" + this.attributeValue(attributes[attName]);
+			}
+			locator += "]";
+			return locator;
+		}
+
+		if (e.attributes) {
+			var atts = e.attributes;
+			var attsMap = {};
+			for (var i = 0; i < atts.length; i++) {
+				var att = atts[i];
+				attsMap[att.name] = att.value;
+			}
+			var names = [];
+			// try preferred attributes
+			for (var i = 0; i < PREFERRED_ATTRIBUTES.length; i++) {
+				var name = PREFERRED_ATTRIBUTES[i];
+				if (attsMap[name] != null) {
+					names.push(name);
+					var locator = attributesXPath.call(this, e.nodeName.toLowerCase(), names, attsMap);
+					if (e == this.findElement(locator)) {
+						return locator;
+					}
+				}
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('hrefXPath', function(e) {
+		if (e.attributes && e.hasAttribute("href")) {
+			href = e.getAttribute("href");
+			if (href.search(/^http?:\/\//) >= 0) {
+				return "//a[@href=" + this.attributeValue(href) + "]";
+			} else {
+				// use contains(), because in IE getAttribute("href") will return absolute path
+				return "//a[contains(@href, " + this.attributeValue(href) + ")]";
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('domFormElementIndex', function(e) {
+		if (e.form) {
+			var formLocator = this.findDomFormLocator(e.form);
+			var elements = e.form.elements;
+			for (var i = 0; i < elements.length; i++) {
+				if (elements[i] == e) {
+					return formLocator + ".elements[" + i + "]";
+				}
+			}
+		}
+		return null;
+	});
+
+LocatorBuilders.add('positionXPath', function(e) {
+		this.log.debug("positionXPath: e=" + e);
+		var path = '';
+		var current = e;
+		while (current != null) {
+			var currentPath = '/' + current.nodeName.toLowerCase();
+			if (current.parentNode != null) {
+				var childNodes = current.parentNode.childNodes;
+				var total = 0;
+				var index = -1;
+				for (var i = 0; i < childNodes.length; i++) {
+					var child = childNodes[i];
+					if (child.nodeName == current.nodeName) {
+						if (child == current) {
+							index = total;
+						}
+						total++;
+					}
+				}
+				if (total > 1 && index >= 0) {
+					currentPath += '[' + (index + 1) + ']';
+				}
+			}
+			path = currentPath + path;
+			var locator = '/' + path;
+			if (e == this.findElement(locator)) {
+				return locator;
+			}
+			current = current.parentNode;
+			this.log.debug("positionXPath: current=" + current);
+		}
+		return null;
+	});
+
+// You can change the priority of builders by setting LocatorBuilders.order.
+//LocatorBuilders.order = ['id', 'link', 'name', 'domFormElementName', 'linkXPath', 'imgXPath', 'attributesXPath', 'hrefXPath', 'domFormElementIndex', 'positionXPath'];
+
