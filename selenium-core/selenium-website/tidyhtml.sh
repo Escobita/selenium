904702ec7fd99adb67b95ce0676300742498ecf4
paul <paul@07704840-8298-11de-bf8c-fd130f914ac9>
2009-01-08 21:10:08 +0000

    remove version numbers
    git-svn-id: http://selenium.googlecode.com/svn@6063 07704840-8298-11de-bf8c-fd130f914ac9

--- selenium-core/selenium-website/tidyhtml.sh
+++ selenium-core/selenium-website/tidyhtml.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 find src/main/webapp -name "*.html" | xargs tidy -i -m
+find src/main/webapp -name "*.html" | xargs perl -p -i -e "s/HTML Tidy for Mac OS X/HTML Tidy/g"
+find src/main/webapp -name "*.html" | xargs perl -p -i -e "s/vers 1 September 2005/version num removed/g"
