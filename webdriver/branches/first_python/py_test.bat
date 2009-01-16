79d440cf955a43c95d251af83ea1a37dbaafa82a
Jiayao.Yu <Jiayao.Yu@07704840-8298-11de-bf8c-fd130f914ac9>
2009-01-16 10:43:52 +0000

    JiayaoYu:py binding:rename the basic_tests.py to api_examples.py
    git-svn-id: http://selenium.googlecode.com/svn@6118 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/first_python/py_test.bat
+++ webdriver/branches/first_python/py_test.bat
@@ -1,3 +1,3 @@
 set PYTHONPATH=%PYTHONPATH%;bindings\py\src;bindings\py\lib 
 set webdriver_test_htmlroot="common/src/web/"
-python bindings\py\tests\basic_tests.py
+python bindings\py\tests\api_examples.py
--- webdriver/branches/first_python/py_tests.sh
+++ webdriver/branches/first_python/py_tests.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:bindings/py/src:bindings/py/lib
 export webdriver_test_htmlroot="common/src/web/"
-python bindings/py/tests/basic_tests.py
+python bindings/py/tests/api_examples.py
