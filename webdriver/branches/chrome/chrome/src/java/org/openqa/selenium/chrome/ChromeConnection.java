730eb5826cf8a1fca1a9fa3c461b995feafa5f27
saikia <saikia@07704840-8298-11de-bf8c-fd130f914ac9>
2009-06-25 21:44:55 +0000

    AmitabhSaikia: Removing old chromium automation attempt. Its clashing with the existing one with Rake. We can retrieve it from the revision tree if we want to.
    git-svn-id: http://selenium.googlecode.com/svn@6836 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/chrome/chrome/src/java/org/openqa/selenium/chrome/ChromeConnection.java
+++
@@ -1,8 +0,0 @@
-package org.openqa.selenium.chrome;
-
-public interface ChromeConnection {
-    boolean isConnected();
-    String executeCommand(Class<? extends RuntimeException> throwOnFailure, String command);
-    String executeJavascript(Class<? extends RuntimeException> throwOnFailure, String script);
-    void quit();
-}
