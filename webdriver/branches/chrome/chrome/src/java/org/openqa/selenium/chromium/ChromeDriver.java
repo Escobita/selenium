707d7e3b07ff8356d626f5c1d45d7bf2af58e638
saikia <saikia@07704840-8298-11de-bf8c-fd130f914ac9>
2009-06-26 01:24:21 +0000

    AmitabhSaikia: Adopt IE like library loading for chrome.
    git-svn-id: http://selenium.googlecode.com/svn@6842 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/chrome/chrome/src/java/org/openqa/selenium/chromium/ChromeDriver.java
+++ webdriver/branches/chrome/chrome/src/java/org/openqa/selenium/chromium/ChromeDriver.java
@@ -14,6 +14,10 @@ import com.sun.jna.WString;
 import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
 import java.util.Set;
 import java.util.List;
 
@@ -43,7 +47,53 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
     if (lib != null) {
       return;
     }
-    lib = (ExportedWebDriver)Native.loadLibrary("ChromeDriver", ExportedWebDriver.class);
+
+    StringBuilder jnaPath = new StringBuilder();
+    jnaPath.append(System.getProperty("java.class.path"));
+    jnaPath.append(File.pathSeparator);
+
+    File dll = writeResourceToDisk("ChromeDriver.dll");
+    dll.deleteOnExit();
+    String driverLib = dll.getName().replace(".dll", "");
+    jnaPath.append(dll.getParent());
+
+    System.setProperty("jna.library.path", jnaPath.toString());
+
+    try {
+      lib = (ExportedWebDriver)  Native.loadLibrary("ChromeDriver", ExportedWebDriver.class);
+    } catch (UnsatisfiedLinkError e) {
+      lib = (ExportedWebDriver)  Native.loadLibrary(driverLib, ExportedWebDriver.class);
+    }
+  }
+
+  private File writeResourceToDisk(String resourceName) throws UnsatisfiedLinkError {
+    InputStream is = ChromeDriver.class.getResourceAsStream(resourceName);
+    if (is == null) 
+      is = ChromeDriver.class.getResourceAsStream("/" + resourceName);
+
+    FileOutputStream fos = null;
+
+    try {
+      File dll = File.createTempFile("webdriver", ".dll");
+      dll.deleteOnExit();
+      fos = new FileOutputStream(dll);
+
+      int count;
+      byte[] buf = new byte[4096];
+      while ((count = is.read(buf, 0, buf.length)) > 0) {
+        fos.write(buf, 0, count);
+      }
+
+      return dll;
+    } catch(IOException e2) {
+      throw new UnsatisfiedLinkError("Cannot create temporary DLL: " + e2.getMessage());
+    }
+    finally {
+      try { is.close(); } catch(IOException e2) { }
+      if (fos != null) {
+        try { fos.close(); } catch(IOException e2) { }
+      }
+    }
   }
 
   @Override
