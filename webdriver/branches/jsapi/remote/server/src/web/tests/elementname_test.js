fc5cc4e652be33119901deb609e2ecff64bbcc4d
jmleyba <jmleyba@07704840-8298-11de-bf8c-fd130f914ac9>
2009-09-06 17:10:51 +0000

    JasonLeyba: Deleting duplicate code. These files are all located in the selenium/ subdirectory (picked up from the merge with trunk in r7221).
    git-svn-id: http://selenium.googlecode.com/svn@7222 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/jsapi/remote/server/src/web/tests/elementname_test.js
+++
@@ -1,5 +0,0 @@
-function testShouldReturnInput(driver) {
-  driver.get(TEST_PAGES.formPage);
-  var element =  driver.findElement({id: 'cheese'});
-  assertThat(element.getElementName(), is('input'));
-}
