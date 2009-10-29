e77892ea84745b751c84ca04fd9d24388eb8d7c8
jmleyba <jmleyba@07704840-8298-11de-bf8c-fd130f914ac9>
2009-10-29 15:57:36 +0000

    JasonLeyba: Deleting an unused branch.
    git-svn-id: http://selenium.googlecode.com/svn@7498 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/jsapi/selenium/src/java/org/openqa/selenium/internal/selenesedriver/SeleneseFunction.java
+++
@@ -1,7 +0,0 @@
-package org.openqa.selenium.internal.selenesedriver;
-
-import com.thoughtworks.selenium.Selenium;
-
-public interface SeleneseFunction<T> {
-  T apply(Selenium selenium, Object... args);
-}
