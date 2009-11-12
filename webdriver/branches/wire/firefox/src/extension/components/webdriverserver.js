06815c22926539b83bc2d90ce8a23e0d2639a912
jmleyba <jmleyba@07704840-8298-11de-bf8c-fd130f914ac9>
2009-11-12 16:48:53 +0000

    JasonLeyba: Cleaning up and adding comments to webdriverserver.js.  I feel like I've done this before on one branch or another...
    git-svn-id: http://selenium.googlecode.com/svn@7593 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/wire/firefox/src/extension/components/webdriverserver.js
+++ webdriver/branches/wire/firefox/src/extension/components/webdriverserver.js
@@ -17,27 +17,62 @@
  */
 
 
+/**
+ * Wraps a {@code nsIServerSocket} for listening for commands to the
+ * FirefoxDriver.
+ * @constructor
+ * @extends {nsIServerSocketListener}
+ */
 function WebDriverServer() {
   this.wrappedJSObject = this;
-  this.serverSocket =
-  Components.classes["@mozilla.org/network/server-socket;1"].
+  this.serverSocket = Components.
+      classes["@mozilla.org/network/server-socket;1"].
       createInstance(Components.interfaces.nsIServerSocket);
-  this.generator =
-  Utils.getService("@mozilla.org/uuid-generator;1", "nsIUUIDGenerator");
+  this.generator = Components.
+      classes["@mozilla.org/uuid-generator;1"].
+      getService(Components.interfaces.nsIUUIDGenerator);
   this.enableNativeEvents = null;
 }
 
 
+/**
+ * The default port the WebDriverServer will listen on.
+ * @type {number}
+ */
+WebDriverServer.DEFAULT_PORT = 7055;
+
+
+/**
+ * Name of the profile preference that defines which port to listen on.
+ * @type {string}
+ */
+WebDriverServer.PORT_PREFERENCE = 'webdriver_firefox_port';
+
+
+/**
+ * Name of the profile preference that defines whether to use native events.
+ * @type {string}
+ */
+WebDriverServer.NATIVE_EVENT_PREFERENCE = 'webdriver_enable_native_events';
+
+
+/**
+ * Creates a new FirefoxDriver for the given {@code window}. The driver will
+ * be accessible as the {@code fxdriver} property.
+ * @param {nsIDOMWIndow} window The window to attach the new driver to.
+ * @return {FirefoxDriver} The new driver instance.
+ */
 WebDriverServer.prototype.newDriver = function(window) {
   if (null == this.useNativeEvents) {
-    var prefs =
-        Utils.getService("@mozilla.org/preferences-service;1", "nsIPrefBranch");
-    if (!prefs.prefHasUserValue("webdriver_enable_native_events")) {
+    var prefs = Components.
+      classes["@mozilla.org/preferences-service;1"].
+      getService(Components.interfaces.nsIPrefBranch);
+    if (!prefs.prefHasUserValue(WebDriverServer.NATIVE_EVENT_PREFERENCE)) {
       Utils.dumpn('webdriver_enable_native_events not set; defaulting to true');
     }
     this.enableNativeEvents =
-    prefs.prefHasUserValue("webdriver_enable_native_events") ?
-    prefs.getBoolPref("webdriver_enable_native_events") : false;
+        prefs.prefHasUserValue(WebDriverServer.NATIVE_EVENT_PREFERENCE) ?
+        prefs.getBoolPref(WebDriverServer.NATIVE_EVENT_PREFERENCE) : false;
     Utils.dumpn('Enable native events: ' + this.enableNativeEvents);
   }
   window.fxdriver = new FirefoxDriver(this, this.enableNativeEvents);
@@ -47,11 +82,23 @@ WebDriverServer.prototype.newDriver = function(window) {
 };
 
 
+/**
+ * Returns a random UUID.
+ * @return {string} The random UUID as a string.
+ */
 WebDriverServer.prototype.getNextId = function() {
   return this.generator.generateUUID().toString();
 };
 
 
+/**
+ * Called when the wrapped {@code nsIServerSocket} has accepted a new
+ * connection. Creates a new {@code SocketListener} to handle the actual
+ * socket I/O.
+ * @param {nsIServerSocket} socket The server socket.
+ * @param {nsISocketTransport} transport The connected socket transport.
+ * @see {nsIServerSocketListener#onSocketAccepted}
+ */
 WebDriverServer.prototype.onSocketAccepted = function(socket, transport) {
   try {
     var socketListener = new SocketListener(transport);
@@ -61,13 +108,23 @@ WebDriverServer.prototype.onSocketAccepted = function(socket, transport) {
 };
 
 
-WebDriverServer.prototype.startListening = function(port) {
+/**
+ * Starts listening on the specified port. The port is chosed in the
+ * following order:
+ * - The argument passed to this function.
+ * - The value of the {@code WebDriverServer.PORT_PREFERENCE}
+ * - {@code WebDriverServer.DEFAULT_PORT}.
+ * @param {number} opt_port The port to listen on.
+ */
+WebDriverServer.prototype.startListening = function(opt_port) {
+  var port = opt_port;
   if (!port) {
-    var prefs =
-        Utils.getService("@mozilla.org/preferences-service;1", "nsIPrefBranch");
-
-    port = prefs.prefHasUserValue("webdriver_firefox_port") ?
-           prefs.getIntPref("webdriver_firefox_port") : 7055;
+    var prefs = Components.
+      classes["@mozilla.org/preferences-service;1"].
+      getService(Components.interfaces.nsIPrefBranch);
+    port = prefs.prefHasUserValue(WebDriverServer.PORT_PREFERENCE) ?
+           prefs.getIntPref(WebDriverServer.PORT_PREFERENCE) :
+           WebDriverServer.DEFAULT_PORT;
   }
 
   if (!this.isListening) {
@@ -78,16 +135,19 @@ WebDriverServer.prototype.startListening = function(port) {
 };
 
 
-WebDriverServer.prototype.onStopListening = function(socket, status)
-{
+/**
+ * Called when the server socket is closed.
+ * @param {nsIServerSocket} socket The closed socket.
+ * @param {nsresult} status Indicates why the connection was closed.
+ * @see {nsIServerSocketListener#onStopListening}
+ */
+WebDriverServer.prototype.onStopListening = function(socket, status) {
 };
 
 
-WebDriverServer.prototype.close = function()
-{
-};
-
-
+/**
+ * @see {nsISupports#QueryInterface} 
+ */
 WebDriverServer.prototype.QueryInterface = function(aIID) {
   if (!aIID.equals(nsISupports))
     throw Components.results.NS_ERROR_NO_INTERFACE;
