Escobita/selenium

commit 3ba3211fe53e2061b18d3d01c380f39db31569af

JasonLeyba: Bringing branch up to head (r7960:r7995)
git-svn-id: http://selenium.googlecode.com/svn@7996 07704840-8298-11de-bf8c-fd130f914ac9

webdriver/branches/wire2/chrome/src/java/org/openqa/selenium/chrome/ChromeWebElement.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.RenderedWebElement;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.internal.FindsByClassName;
+import org.openqa.selenium.internal.FindsByCssSelector;
 import org.openqa.selenium.internal.FindsById;
 import org.openqa.selenium.internal.FindsByLinkText;
 import org.openqa.selenium.internal.FindsByName;
@@ -21,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ChromeWebElement implements RenderedWebElement, Locatable, 
-FindsByXPath, FindsByLinkText, FindsById, FindsByName, FindsByTagName, FindsByClassName {
+FindsByXPath, FindsByLinkText, FindsById, FindsByName, FindsByTagName, FindsByClassName, FindsByCssSelector {
 
   private final ChromeDriver parent;
   private final String elementId;
@@ -207,6 +208,14 @@ FindsByXPath, FindsByLinkText, FindsById, FindsByName, FindsByTagName, FindsByCl
 
   public List<WebElement> findElementsByClassName(String using) {
     return findElements("class name", using);
+  }
+  
+  public WebElement findElementByCssSelector(String using) {
+    return parent.getElementFrom(execute(DriverCommand.FIND_CHILD_ELEMENT, this, "css", using));
+  }
+
+  public List<WebElement> findElementsByCssSelector(String using) {
+    return parent.getElementsFrom(execute(DriverCommand.FIND_CHILD_ELEMENTS, this, "css", using));
   }
 
   public void hover() {

webdriver/branches/wire2/Rakefile
@@ -350,6 +350,15 @@ java_jar(:name => "webdriver-remote-client",
                "remote/client/lib/runtime/*.jar",
              ])
 
+java_jar(:name => "selenium-common-js",
+    :resources => [
+      "common/src/js/core",
+      "common/src/js/jsunit",
+      {
+        "common/src/js/core/TestRunner.html" => "core/TestRunner.hta",
+        "common/src/js/core/RemoteRunner.html" => "core/RemoteRunner.hta",
+      }])
+
 java_jar(:name => "webdriver-remote-server",
     :srcs  => [ "remote/server/src/java/**/*.java" ],
     :resources => [

webdriver/branches/wire2/chrome/src/extension/background.js
@@ -509,8 +509,8 @@ function parseRequest(request) {
       sendResponseToParsedRequest({statusCode: 0, value: []});
       break;
     }
-  // Falling through, as if we do have a page, we want to treat this like a
-  // normal request
+    // Falling through, as if we do have a page, we want to treat this like a
+    // normal request
   case "deleteAllCookies":
   case "deleteCookie":
     if (hasNoPage()) {
@@ -518,8 +518,16 @@ function parseRequest(request) {
       sendResponseToParsedRequest({statusCode: 0});
       break;
     }
-  // Falling through, as if we do have a page, we want to treat this like a
-  // normal request
+    // Falling through, as if we do have a page, we want to treat this like a
+    // normal request
+  case "executeScript":
+    if (hasNoPage()) {
+      console.log("Not got a page, but asked to execute script, so sending error 17");
+      sendResponseToParsedRequest({statusCode: 17, value: {message: 'Was not on a page, so could not execute javascript'}});
+      break;
+    }
+    // Falling through, as if we do have a page, we want to treat this like a
+    // normal request
   default:
     var sequenceNumber = ChromeDriver.requestSequenceNumber;
     ChromeDriver.requestSequenceNumber++;

webdriver/branches/wire2/chrome/src/extension/content_script.js
@@ -457,6 +457,9 @@ function getElement(plural, lookupBy, lookupValue, id) {
   case "tag name":
     elements = getElementsByXPath(root + "//" + lookupValue);
     break;
+  case "css":
+    elements = parent.querySelectorAll(lookupValue);
+    break;
   case "xpath":
     if (root != "" && lookupValue[0] != "/") {
       //Because xpath is relative to the parent, if there is a parent, and the lookup seems to be implied sub-lookup, we add a /
@@ -716,10 +719,8 @@ function sendElementKeys(element, keys, elementId) {
   if (oldFocusedElement != element) {
     //TODO: Work out a way of firing events,
     //now that synthesising them gives appendMessage errors
-    oldFocusedElement.blur();
-    Utils.fireHtmlEvent(oldFocusedElement, "blur");
-    element.focus();
-    Utils.fireHtmlEvent(element, "focus");
+    Utils.fireHtmlEventAndConditionallyPerformAction(oldFocusedElement, "blur", function() {oldFocusedElement.blur();});
+    Utils.fireHtmlEventAndConditionallyPerformAction(element, "focus", function() {element.focus();});
   }
   return {statusCode: "no-op", keys: keys, id: elementId};
 }
@@ -741,7 +742,7 @@ function sendElementNonNativeKeys(element, keys) {
 function submitElement(element) {
   while (element != null) {
     if (element.tagName.toLowerCase() == "form") {
-      element.submit();
+      Utils.fireHtmlEventAndConditionallyPerformAction(element, "submit", function() {element.submit();});
       return {statusCode: 0};
     }
     element = element.parentNode;
@@ -870,7 +871,8 @@ function parseWrappedArguments(argument) {
  * We can't share objects between content script and page, so have to wrap up arguments as JSON
  * @param script script to execute as a string
  * @param passedArgs array of arguments to pass to the script
- * @param callback function to call when the result is returned
+ * @param callback function to call when the result is returned.  Passed a DOMAttrModified event which should be parsed as returnFromJavascriptInPage
+ * TODO: Make the callback be passed the parsed result.
  */
 function execute_(script, passedArgs, callback) {
   console.log("executing " + script + ", args: " + JSON.stringify(passedArgs));

webdriver/branches/wire2/chrome/src/extension/utils.js
@@ -174,7 +174,18 @@ Utils.getText = function(element) {
     return text.slice(start, end);
 };
 
-Utils.fireHtmlEvent = function(element, eventName) {
+/**
+ * Fires the event using Utils.fireEvent, and if the event returned true,
+ * perform callback, which will be passed on arguments
+ */
+Utils.fireHtmlEventAndConditionallyPerformAction = function(element, eventName, callback) {
+    Utils.fireHtmlEvent(element, eventName, function(evt) { if (JSON.parse(evt.newValue).value) { callback(); } });
+};
+
+Utils.fireHtmlEvent = function(element, eventName, callback) {
+    if (callback === undefined) {
+      callback = function() {};
+    }
     var args = [
       {type: "ELEMENT", value: addElementToInternalArray(element)},
       {type: "STRING", value: eventName}
@@ -185,9 +196,9 @@ Utils.fireHtmlEvent = function(element, eventName) {
     // http://code.google.com/p/chromium/issues/detail?id=29071
     var script = "var e = document.createEvent('HTMLEvents'); "
       + "e.initEvent(arguments[1], true, true); " 
-      + "arguments[0].dispatchEvent(e);";
+      + "return arguments[0].dispatchEvent(e);";
 
-    execute_(script, args, function(){});
+    execute_(script, args, callback);
 };
 
 

webdriver/branches/wire2/chrome/src/java/org/openqa/selenium/chrome/ChromeBinary.java
@@ -1,35 +1,47 @@
 package org.openqa.selenium.chrome;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriverException;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ChromeBinary {
   
   private static final int BACKOFF_INTERVAL = 2500;
 
   private static int linearBackoffCoefficient = 1;
+
+  private final ChromeProfile profile;
+  private final ChromeExtension extension;
   
   Process chromeProcess = null;
-  
+
+  /**
+   * Creates a new instance for managing an instance of Chrome using the given
+   * {@code profile} and {@code extension}.
+   *
+   * @param profile The Chrome profile to use.
+   * @param extension The extension to launch Chrome with.
+   */
+  public ChromeBinary(ChromeProfile profile, ChromeExtension extension) {
+    this.profile = profile;
+    this.extension = extension;
+  }
+
   /**
    * Starts the Chrome process for WebDriver.
    * Assumes the passed directories exist.
-   * @param profileDir directory to use as the profile.
-   * Should contain the empty text file "First Run Dev".
-   * @param extensionDir directory which contains the WebDriver extension.
    * @param serverUrl URL from which commands should be requested
    * @throws IOException wrapped in WebDriverException if process couldn't be
    * started.
    */
-  public void start(String profileDir, String extensionDir, String serverUrl) throws IOException {
+  public void start(String serverUrl) throws IOException {
     try {
       chromeProcess = new ProcessBuilder(
           getChromeFile(),
-          "--user-data-dir=" + profileDir,
-          "--load-extension=" + extensionDir,
+          "--user-data-dir=" + profile.getDirectory().getAbsolutePath(),
+          "--load-extension=" + extension.getDirectory().getAbsolutePath(),
           "--activate-on-launch",
           "--homepage=about:blank",
           "--no-first-run",

webdriver/branches/wire2/chrome/src/java/org/openqa/selenium/chrome/ChromeCommandExecutor.java
@@ -38,14 +38,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ChromeCommandExecutor {
   private static final String[] ELEMENT_ID_ARG = new String[] {"id"};
   private static final String[] NO_ARGS = new String[] {};
-  
-  private final ServerSocket serverSocket;
+
+  private final int port;
   //Whether the listening thread should listen
   private volatile boolean listen = false;
-  //Whether a client is currently connected
-  private boolean hadClient = false;
-  //Whether a client has ever been connected
-  private boolean hasClient = false;
   ListeningThread listeningThread;
   private Map<DriverCommand, String[]> commands;
   
@@ -57,7 +53,7 @@ public class ChromeCommandExecutor {
   public ChromeCommandExecutor() {
     this(0);
   }
-  
+
   /**
    * Creates a new ChromeCommandExecutor which listens on the passed TCP port.
    * Doesn't return until the TCP port is connected to.
@@ -66,6 +62,7 @@ public class ChromeCommandExecutor {
    * @throws WebDriverException if could not bind to port
    */
   public ChromeCommandExecutor(int port) {
+    this.port = port;
     commands = ImmutableMap.<DriverCommand, String[]> builder()
         .put(CLOSE, NO_ARGS)
         .put(QUIT, NO_ARGS)
@@ -114,23 +111,14 @@ public class ChromeCommandExecutor {
         .put(EXECUTE_SCRIPT, new String[] {"script", "args"})
         .put(SCREENSHOT, NO_ARGS)
         .build();
-
-    try {
-      serverSocket = new ServerSocket(port);
-    } catch (IOException e) {
-      throw new WebDriverException(e);
-    }
-    listen = true;
-    listeningThread = new ListeningThread(serverSocket);
-    listeningThread.start();
   }
-  
+
   /**
    * Returns whether an instance of Chrome is currently connected
    * @return whether an instance of Chrome is currently connected
    */
   boolean hasClient() {
-    return hasClient;
+    return listeningThread != null && listeningThread.hasClient;
   }
   
   /**
@@ -138,7 +126,7 @@ public class ChromeCommandExecutor {
    * @return the port being listened on
    */
   public int getPort() {
-    return serverSocket.getLocalPort();
+    return listeningThread == null ? -1 : listeningThread.serverSocket.getLocalPort();
   }
   
   /**
@@ -159,16 +147,10 @@ public class ChromeCommandExecutor {
    * @throws IOException if couldn't write command to socket
    */
   private void sendCommand(Command command) throws IOException {
-    Socket socket;
-    //Peek, rather than poll, so that if it all goes horribly wrong,
-    //we can just close all sockets in the queue,
-    //not having to worry about the current ones
-    while ((socket = listeningThread.sockets.peek()) == null) {
-      if (hadClient && !hasClient) {
-        throw new IllegalStateException("Cannot execute command without a client");
-      }
-      Thread.yield();
+    if (!hasClient()) {
+      throw new IllegalStateException("Cannot execute command without a client");
     }
+    Socket socket = getOldestSocket();
     try {
       //Respond to request with the command
       String commandStringToSend;
@@ -278,13 +260,7 @@ public class ChromeCommandExecutor {
    * @throws IOException if there are errors with the socket being used
    */
   private Response handleResponse(Command command) throws IOException {
-    Socket socket;
-    //Peek, rather than poll, so that if it all goes horribly wrong,
-    //we can just close all sockets in the queue,
-    //not having to worry about the current ones
-    while ((socket = listeningThread.sockets.peek()) == null) {
-      Thread.yield();
-    }
+    Socket socket = getOldestSocket();
     StringBuilder resultBuilder = new StringBuilder();
     BufferedReader reader = new BufferedReader(
         new InputStreamReader(socket.getInputStream()));
@@ -303,6 +279,17 @@ public class ChromeCommandExecutor {
       }
     }
     return parseResponse(resultBuilder.toString());
+  }
+
+  private Socket getOldestSocket() {
+    Socket socket;
+    // Peek, rather than poll, so that if it all goes horribly wrong, we can
+    // just close all sockets in the queue, not having to worry about the
+    // current ones.
+    while ((socket = listeningThread.sockets.peek()) == null) {
+      Thread.yield();
+    }
+    return socket;
   }
 
   /**
@@ -458,13 +445,29 @@ public class ChromeCommandExecutor {
   }
 
   /**
+   * Starts listening for new socket connections from Chrome. Does not return
+   * until the TCP port is connected to.
+   */
+  public void startListening() {
+    ServerSocket serverSocket;
+    try {
+      serverSocket = new ServerSocket(port);
+    } catch (IOException e) {
+      throw new WebDriverException(e);
+    }
+    listen = true;
+    listeningThread = new ListeningThread(serverSocket);
+    listeningThread.start();
+  }
+
+  /**
    * Stops listening from for new sockets from Chrome
    */
   public void stopListening() {
     listen = false;
-    listeningThread.stopListening();
-    while (!serverSocket.isClosed()) {// || serverSocket.isBound()) {
-      Thread.yield();
+    if (listeningThread != null) {
+      listeningThread.stopListening();
+      listeningThread = null;
     }
   }
 
@@ -476,11 +479,13 @@ public class ChromeCommandExecutor {
     private boolean isListening = false;
     private Queue<Socket> sockets = new ConcurrentLinkedQueue<Socket>();
     private ServerSocket serverSocket;
-    
+    private volatile boolean hasClient = false;
+
     public ListeningThread(ServerSocket serverSocket) {
       this.serverSocket = serverSocket;
     }
-    
+
+    @Override
     public void run() {
       if (!isListening) {
         listen();
@@ -489,7 +494,7 @@ public class ChromeCommandExecutor {
     public void listen() {
       isListening = true;
       try {
-        while (listen) {
+        while (listen && !serverSocket.isClosed()) {
           Socket acceptedSocket = serverSocket.accept();
           int r = acceptedSocket.getInputStream().read();
           if (r != 'G') {
@@ -499,7 +504,6 @@ public class ChromeCommandExecutor {
             //which we assume to be POSTs from the extension
             sockets.add(acceptedSocket);
             hasClient = true;
-            hadClient = true;
           } else {
             //The browser, rather than extension, is visiting the page
             //Because the extension always uses POST
@@ -508,11 +512,7 @@ public class ChromeCommandExecutor {
           }
         }
       } catch (SocketException e) {
-        if (listen) {
-          throw new WebDriverException(e);
-        } else {
-          //We are shutting down sockets manually
-        }
+        //We are shutting down sockets manually
       } catch (IOException e) {
         isListening = false;
         throw new WebDriverException(e);
@@ -538,17 +538,21 @@ public class ChromeCommandExecutor {
         try {
           if (!serverSocket.isClosed()) {
             serverSocket.close();
+            while (!serverSocket.isClosed()) {
+              Thread.yield();
+            }
           }
         } catch (Exception e) {
           e.printStackTrace();
         }
       }
     }
+
     private void closeCurrentSockets() {
-      for (Socket socket : listeningThread.sockets) {
+      for (Socket socket : sockets) {
         try {
           socket.close();
-          listeningThread.sockets.remove(socket);
+          sockets.remove(socket);
         } catch (IOException e) {
           //Nothing we can sanely do here
         }

webdriver/branches/wire2/chrome/src/java/org/openqa/selenium/chrome/ChromeDriver.java
@@ -5,21 +5,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.Speed;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.internal.FileHandler;
 import org.openqa.selenium.internal.FindsByClassName;
+import org.openqa.selenium.internal.FindsByCssSelector;
 import org.openqa.selenium.internal.FindsById;
 import org.openqa.selenium.internal.FindsByLinkText;
 import org.openqa.selenium.internal.FindsByName;
 import org.openqa.selenium.internal.FindsByTagName;
 import org.openqa.selenium.internal.FindsByXPath;
-import org.openqa.selenium.internal.TemporaryFilesystem;
 import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.Context;
 import org.openqa.selenium.remote.DriverCommand;
@@ -29,8 +27,6 @@ import static org.openqa.selenium.remote.DriverCommand.*;
 import org.openqa.selenium.remote.Response;
 import org.openqa.selenium.remote.SessionId;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -40,38 +36,35 @@ import java.util.Map;
 import java.util.Set;
 
 public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecutor, TakesScreenshot,
-  FindsById, FindsByClassName, FindsByLinkText, FindsByName, FindsByTagName, FindsByXPath {
+  FindsById, FindsByClassName, FindsByLinkText, FindsByName, FindsByTagName, FindsByXPath, FindsByCssSelector {
 
   private final static int MAX_START_RETRIES = 5;
-  private ChromeCommandExecutor executor;
-  private ChromeBinary chromeBinary = new ChromeBinary();
-  private final ErrorHandler errorHandler = new ErrorHandler();
-  
+  private final ChromeCommandExecutor executor;
+  private final ChromeBinary chromeBinary;
+  private final ErrorHandler errorHandler;
+
+  /**
+   * Starts up a new instance of Chrome using the specified profile and
+   * extension.
+   *
+   * @param profile The profile to use.
+   * @param extension The extension to use.
+   */
+  public ChromeDriver(ChromeProfile profile, ChromeExtension extension) {
+    chromeBinary = new ChromeBinary(profile, extension);
+    executor = new ChromeCommandExecutor();
+    errorHandler = new ErrorHandler();
+    startClient();
+  }
+
   /**
    * Starts up a new instance of Chrome, with the required extension loaded,
    * and has it connect to a new ChromeCommandExecutor on its port
+   *
+   * @see ChromeDriver(ChromeProfile, ChromeExtension)
    */
   public ChromeDriver() {
-    init();
-  }
-  
-  private void init() {
-    int retries = MAX_START_RETRIES;
-    while ((executor == null || !executor.hasClient()) && retries > 0) {
-      stopClient();
-      this.executor = new ChromeCommandExecutor();
-      startClient();
-      //In case this attempt fails, we increment how long we wait before sending a command
-      chromeBinary.incrementBackoffBy(1);
-      retries--;
-    }
-    //The last one attempt succeeded, so we reduce back to that time
-    chromeBinary.incrementBackoffBy(-1);
-
-    if (executor == null || !executor.hasClient()) {
-      stopClient();
-      throw new FatalChromeException("Cannot create chrome driver");  
-    }
+    this(new ChromeProfile(), new ChromeExtension());
   }
   
   /**
@@ -82,35 +75,23 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
    * hope we're in.  If these fail, throws exceptions.
    */
   protected void startClient() {
-    try {
-      File extensionDir = getExtensionDir();
-      if (!extensionDir.isDirectory()) {
-        throw new FileNotFoundException("Could not find extension directory" +
-            "(" + extensionDir + ").  Try setting webdriver.chrome.extensiondir."); 
+    for (int retries = MAX_START_RETRIES; !executor.hasClient() && retries > 0; retries--) {
+      stopClient();
+      try {
+        executor.startListening();
+        chromeBinary.start(getServerUrl());
+      } catch (IOException e) {
+        throw new WebDriverException(e);
       }
-      
-      //Copy over the correct manifest file
-      if (Platform.getCurrent().is(Platform.WINDOWS)) {
-        FileHandler.copy(new File(extensionDir, "manifest-win.json"),
-                         new File(extensionDir, "manifest.json"));
-      } else {
-        FileHandler.copy(new File(extensionDir, "manifest-nonwin.json"),
-                         new File(extensionDir, "manifest.json"));
-      }
-      
-      File profileDir = TemporaryFilesystem.createTempDir("profile", "");
-      File firstRunFile = new File(profileDir, "First Run Dev");
-      firstRunFile.createNewFile();
-      //TODO(danielwh): Maybe add Local State file with window_placement
-      
-      System.setProperty("webdriver.reap_profile", "false");
-      
-      chromeBinary.start(
-          profileDir.getCanonicalFile().toString(),
-          extensionDir.getCanonicalFile().toString(),
-          getServerUrl());
-    } catch (IOException e) {
-      throw new WebDriverException(e);
+      //In case this attempt fails, we increment how long we wait before sending a command
+      chromeBinary.incrementBackoffBy(1);
+    }
+    //The last one attempt succeeded, so we reduce back to that time
+    chromeBinary.incrementBackoffBy(-1);
+
+    if (!executor.hasClient()) {
+      stopClient();
+      throw new FatalChromeException("Cannot create chrome driver");
     }
   }
   
@@ -119,10 +100,7 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
    */
   protected void stopClient() {
     chromeBinary.kill();
-    if (executor != null) {
-      executor.stopListening();
-      executor = null;
-    }
+    executor.stopListening();
   }
   
   /**
@@ -144,7 +122,7 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
         //These exceptions may leave the extension hung, or in an
         //inconsistent state, so we restart Chrome
         stopClient();
-        init();
+        startClient();
       }
       if (e instanceof RuntimeException) {
         throw (RuntimeException)e;
@@ -163,35 +141,8 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
     return execute(driverCommand, ImmutableMap.<String, Object>of());
   }
   
-  /**
-   * Locates the directory containing the extension to load Chrome with,
-   * trying to unzip the zipped extension if no explicit extension is set using
-   * the system property webdriver.chrome.extensiondir.
-   * @return the extension directory
-   * @throws IOException if tried to unzip extension but couldn't
-   */
-  protected File getExtensionDir() throws IOException {
-    File extensionDir = null;
-    String extensionDirSystemProperty = System.getProperty(
-        "webdriver.chrome.extensiondir");
-    if (extensionDirSystemProperty != null &&
-        extensionDirSystemProperty != "") {
-      //Default to reading from the property
-      extensionDir = new File(extensionDirSystemProperty);
-    } else {
-      //If property not set, try to unpack the zip from the jar
-      extensionDir = FileHandler.unzip(this.getClass().getResourceAsStream(
-          "/chrome-extension.zip"));
-    }
-    return extensionDir;
-  }
-  
   protected String getServerUrl() {
-    if (executor != null) {
-      return "http://localhost:" + executor.getPort() + "/chromeCommandExecutor";
-    } else {
-      throw new WebDriverException("No server started - try initing the ChromeDriver");
-    }
+    return "http://localhost:" + executor.getPort() + "/chromeCommandExecutor";
   }
 
   public void close() {
@@ -323,6 +274,14 @@ public class ChromeDriver implements WebDriver, SearchContext, JavascriptExecuto
 
   public List<WebElement> findElementsByPartialLinkText(String using) {
     return findElements("partial link text", using);
+  }
+  
+  public WebElement findElementByCssSelector(String using) {
+    return findElement("css", using);
+  }
+
+  public List<WebElement> findElementsByCssSelector(String using) {
+    return findElements("css", using);
   }
 
   private WebElement findElement(String by, String using) {

commit 02c00cdf285fdbf234761faf67a4a1dd19d332cf

JasonLeyba: Fixing broken build from r7996
git-svn-id: http://selenium.googlecode.com/svn@7998 07704840-8298-11de-bf8c-fd130f914ac9

webdriver/branches/wire2/chrome/src/java/org/openqa/selenium/chrome/ChromeWebElement.java
@@ -211,11 +211,11 @@ FindsByXPath, FindsByLinkText, FindsById, FindsByName, FindsByTagName, FindsByCl
   }
   
   public WebElement findElementByCssSelector(String using) {
-    return parent.getElementFrom(execute(DriverCommand.FIND_CHILD_ELEMENT, this, "css", using));
+    return findElement("css", using);
   }
 
   public List<WebElement> findElementsByCssSelector(String using) {
-    return parent.getElementsFrom(execute(DriverCommand.FIND_CHILD_ELEMENTS, this, "css", using));
+    return findElements("css", using);
   }
 
   public void hover() {