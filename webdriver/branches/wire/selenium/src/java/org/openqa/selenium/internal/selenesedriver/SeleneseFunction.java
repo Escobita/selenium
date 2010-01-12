e6b8492606d7136a9424ad2c4aec9e245c53126f
jmleyba <jmleyba@07704840-8298-11de-bf8c-fd130f914ac9>
2010-01-12 18:03:15 +0000

    JasonLeyba: Deleting old, out-of-date, and unused branch.
    git-svn-id: http://selenium.googlecode.com/svn@8061 07704840-8298-11de-bf8c-fd130f914ac9

--- webdriver/branches/wire/selenium/src/java/org/openqa/selenium/internal/selenesedriver/SeleneseFunction.java
+++
@@ -1,9 +0,0 @@
-package org.openqa.selenium.internal.selenesedriver;
-
-import com.thoughtworks.selenium.Selenium;
-
-import java.util.Map;
-
-public interface SeleneseFunction<T> {
-  T apply(Selenium selenium, Map<String, ?> parameters);
-}
