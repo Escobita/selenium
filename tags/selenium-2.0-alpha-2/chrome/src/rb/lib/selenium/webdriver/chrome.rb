43c5d386ecf306fbfba9d6536b97a1933a45ceeb
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-02-03 13:23:01 +0000

    SimonStewart: Deleting the tag for now because it doesn't include the updated javadocse
    git-svn-id: http://selenium.googlecode.com/svn@8264 07704840-8298-11de-bf8c-fd130f914ac9

--- tags/selenium-2.0-alpha-2/chrome/src/rb/lib/selenium/webdriver/chrome.rb
+++
@@ -1,8 +0,0 @@
-require "selenium/webdriver/chrome/launcher"
-require "selenium/webdriver/chrome/command_executor"
-require "selenium/webdriver/chrome/bridge"
-
-require "fileutils"
-require "thread"
-require "socket"
-
