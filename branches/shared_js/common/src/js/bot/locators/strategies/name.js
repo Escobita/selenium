ae9ddf4a2c0cd6ac7acf7706d1821c53c9b9b6c6
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-03-21 23:19:13 +0000

    SimonStewart: Making the name locator work as expected on IE.
    git-svn-id: http://selenium.googlecode.com/svn@8541 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/shared_js/common/src/js/bot/locators/strategies/name.js
+++ branches/shared_js/common/src/js/bot/locators/strategies/name.js
@@ -1,20 +1,38 @@
 
 goog.provide('bot.locators.strategies.name');
 
+goog.require('bot.dom');
+goog.require('goog.array');
 
-bot.locators.strategies.name = function(win, name) {
+/**
+ * Find an element by the value of the name attribute
+ *
+ * @param {Window} win The DOM window to search in.
+ * @param {string} target The id to search for.
+ * @return {?Element} The first matching element found in the DOM, or null if no
+ *     such element could be found.
+ */
+bot.locators.strategies.name = function(win, target) {
   var doc = win.document;
 
   if (doc['getElementsByName']) {
-    var results = doc.getElementsByName(name);
+    var results = doc.getElementsByName(target);
 
     var length = results.length;
     for (var i = 0; i < length; i++) {
-      if (results[i].getAttribute('name') === name) {
+      if (results[i].getAttribute('name') === target) {
         return results[i];
       }
     }
   }
 
-  return undefined;  
+  // On some browsers (notably IE) "getElementsByName" only returns elements
+  // that are in a form. If we get here, it's possible a matching element is in
+  // the DOM, but not in a form. Hunt for it.
+
+  var allElements = doc.getElementsByTagName('*');
+
+  return goog.array.find(allElements, function(element) {
+    return bot.dom.getAttribute(element, 'name') == target;
+  })
 };
