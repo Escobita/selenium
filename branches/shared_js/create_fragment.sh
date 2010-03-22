8e5d3a0f1ad293d32ca08ce623097ab8ee0d9ba2
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-03-22 05:58:32 +0000

    SimonStewart: Fixing a thinko in the create_fragment script
    git-svn-id: http://selenium.googlecode.com/svn@8550 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/shared_js/create_fragment.sh
+++ branches/shared_js/create_fragment.sh
@@ -12,16 +12,16 @@ OUTPUT=$4
 [ -d build ] || mkdir build
 
 # Create the source file
-echo "goog.require('${REQUIRE}'); goog.exportSymbol('${METHOD}', ${CALLABLE});" >> build/${OUTPUT}.tmp.js
+echo "goog.require('${REQUIRE}'); goog.exportSymbol('${CALLABLE}', ${METHOD});" >> build/${OUTPUT}.tmp.js
 
 python third_party/closure/bin/calcdeps.py \
-   -c third_party/closure/bin/compiler-20100201.jar \
-   -o compiled \
-   -f "--third_party=true" \
-   -f "--js_output_file=build/${OUTPUT}.js" \
-   -f "--compilation_level=ADVANCED_OPTIMIZATIONS" \
-   -p third_party/closure/goog/ \
-   -p common/src/js \
-   -i build/${OUTPUT}.tmp.js
+  -c third_party/closure/bin/compiler-20100201.jar \
+  -o compiled \
+  -f "--third_party=true" \
+  -f "--js_output_file=build/${OUTPUT}.js" \
+  -f "--compilation_level=ADVANCED_OPTIMIZATIONS" \
+  -p third_party/closure/goog/ \
+  -p common/src/js \
+  -i build/${OUTPUT}.tmp.js
 
 rm build/${OUTPUT}.tmp.js
