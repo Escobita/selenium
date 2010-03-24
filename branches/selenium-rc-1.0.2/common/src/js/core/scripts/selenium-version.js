04ed6d3cb7d9518ed0b45d8cb93864dbf604d0b4
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-03-24 18:36:59 +0000

    SimonStewart: Deleteing old branches.
    git-svn-id: http://selenium.googlecode.com/svn@8571 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/selenium-rc-1.0.2/common/src/js/core/scripts/selenium-version.js
+++
@@ -1,5 +0,0 @@
-Selenium.version = "2.0";
-Selenium.revision = "a1";
-
-window.top.document.title += " v" + Selenium.version + " [" + Selenium.revision + "]";
-
--- branches/wire/common/src/js/core/scripts/selenium-version.js
+++
@@ -1,5 +0,0 @@
-Selenium.version = "2.0";
-Selenium.revision = "a2";
-
-window.top.document.title += " v" + Selenium.version + " [" + Selenium.revision + "]";
-
