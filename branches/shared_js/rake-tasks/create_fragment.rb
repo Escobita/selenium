a7543a113044fce26ae6dd124621db1a5c04b435
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-03-27 23:43:58 +0000

    SimonStewart: Creating fragments via ruby so as to allow the build to work on Windows
    git-svn-id: http://selenium.googlecode.com/svn@8595 07704840-8298-11de-bf8c-fd130f914ac9

---
+++ branches/shared_js/rake-tasks/create_fragment.rb
@@ -0,0 +1,32 @@
+
+# Compile a single JS method into a compressed fragment
+
+def js_fragment(args)
+  out = "build/#{args[:name]}.js"
+  temp = "build/#{args[:name]}.tmp.js"
+  
+  file out do
+    mkdir_p File.dirname(out)
+    
+    rm_f "#{out}.tmp"
+    
+    File.open(temp, "w") do |file|
+      file << "goog.require('#{args[:module]}'); goog.exportSymbol('#{args[:name]}', #{args[:function]});"
+    end
+    
+    cmd = ""
+    cmd << "python third_party/closure/bin/calcdeps.py "
+    cmd << "-c third_party/closure/bin/compiler-20100201.jar "
+    cmd << "-o compiled "
+    cmd << "-f \"--third_party=true\" "
+    cmd << "-f \"--js_output_file=#{out}\" "
+    cmd << "-f \"--compilation_level=ADVANCED_OPTIMIZATIONS\" "
+    cmd << "-p third_party/closure/goog/ "
+    cmd << "-p common/src/js "
+    cmd << "-i #{temp}"
+    
+    sh cmd
+    
+    rm_f "#{out}.tmp"
+  end
+end
