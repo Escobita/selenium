d3e6c023a5b7756e10ac3fa119cc4aa41af4f152
eran.mes <eran.mes@07704840-8298-11de-bf8c-fd130f914ac9>
2010-04-12 13:32:10 +0000

    EranMes: Merging from trunk to get CapabilityType class
    git-svn-id: http://selenium.googlecode.com/svn@8679 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/interactions_api/firefox/src/java/org/openqa/selenium/firefox/FirefoxDriver.java
+++ branches/interactions_api/firefox/src/java/org/openqa/selenium/firefox/FirefoxDriver.java
@@ -20,6 +20,9 @@ package org.openqa.selenium.firefox;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.BindException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -34,10 +37,14 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.internal.ExtensionConnectionFactory;
+import org.openqa.selenium.firefox.internal.Lock;
+import org.openqa.selenium.firefox.internal.NewProfileExtensionConnection;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.firefox.internal.SocketLock;
 import org.openqa.selenium.internal.FileHandler;
 import org.openqa.selenium.internal.FindsByCssSelector;
+import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.CommandExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -50,53 +57,70 @@ import static org.openqa.selenium.OutputType.FILE;
  * An implementation of the {#link WebDriver} interface that drives Firefox. This works through a firefox extension,
  * which gets installed automatically if necessary. Important system variables are:
  * <ul>
- *  <li><b>webdriver.firefox.bin</b> - Which firefox binary to use (normally "firefox" on the PATH).</li>
- *  <li><b>webdriver.firefox.profile</b> - The name of the profile to use (normally "WebDriver").</li>
+ * <li><b>webdriver.firefox.bin</b> - Which firefox binary to use (normally "firefox" on the PATH).</li>
+ * <li><b>webdriver.firefox.profile</b> - The name of the profile to use (normally "WebDriver").</li>
  * </ul>
- *
+ * <p/>
  * When the driver starts, it will make a copy of the profile it is using, rather than using that profile directly.
  * This allows multiple instances of firefox to be started.
  */
 public class FirefoxDriver extends RemoteWebDriver implements TakesScreenshot, FindsByCssSelector {
+  public static final int DEFAULT_PORT = 7055;
+  // For now, only enable native events on Windows
+  public static final boolean DEFAULT_ENABLE_NATIVE_EVENTS =
+      Platform.getCurrent()
+          .is(Platform.WINDOWS);
+  // Accept untrusted SSL certificates.
+  public static final boolean ACCEPT_UNTRUSTED_CERTIFICATES = true;
+  // Assume that the untrusted certificates will come from untrusted issuers
+  // or will be self signed.
+  public static final boolean ASSUME_UNTRUSTED_ISSUER = true;
 
-    public static final int DEFAULT_PORT = 7055;
-    // For now, only enable native events on Windows
-    public static final boolean DEFAULT_ENABLE_NATIVE_EVENTS =
-      Platform.getCurrent().is(Platform.WINDOWS);
-    // Accept untrusted SSL certificates.
-    public static final boolean ACCEPT_UNTRUSTED_CERTIFICATES = true;
-    // Assume that the untrusted certificates will come from untrusted issuers
-    // or will be self signed.
-    public static final boolean ASSUME_UNTRUSTED_ISSUER = true;
+  // Commands we can execute with needing to dismiss an active alert
+  private final Set<DriverCommand> alertWhiteListedCommands = new HashSet<DriverCommand>() {{
+    add(DriverCommand.DISMISS_ALERT);
+  }};
 
-    // Commands we can execute with needing to dismiss an active alert
-    private final Set<DriverCommand> alertWhiteListedCommands = new HashSet<DriverCommand>() {{
-      add(DriverCommand.DISMISS_ALERT);
-    }};
+  private FirefoxAlert currentAlert;
 
-    private FirefoxAlert currentAlert;
+
+  private final LazyCommandExecutor executor;
+  protected FirefoxBinary binary;
+  protected FirefoxProfile profile;
 
   public FirefoxDriver() {
-      this(new FirefoxBinary(), null);
-    }
-
-  public FirefoxDriver(FirefoxProfile profile) {
-      this(new FirefoxBinary(), profile);
-    }
-
-  public FirefoxDriver(FirefoxBinary binary, FirefoxProfile profile) {
-    super(createExtensionConnection(binary, profile), DesiredCapabilities.firefox());
+    this(new FirefoxBinary(), null);
   }
 
-  /**
-   * Establishes a connection to the Firefox extension.
-   *
-   * @param binary The binary to use for launching Firefox.
-   * @param profile The profile template to launch Firefox with.
-   * @return The established extension connection.
-   */
-  private static ExtensionConnection createExtensionConnection(FirefoxBinary binary,
-                                                               FirefoxProfile profile) {
+  public FirefoxDriver(FirefoxProfile profile) {
+    this(new FirefoxBinary(), profile);
+  }
+
+  public FirefoxDriver(FirefoxBinary binary, FirefoxProfile profile) {
+    super(new LazyCommandExecutor(binary, profile), DesiredCapabilities.firefox());
+    this.binary = binary;
+    this.profile = profile;
+    executor = (LazyCommandExecutor) getCommandExecutor();
+  }
+
+  @Override
+  protected void startClient() {
+    LazyCommandExecutor executor = (LazyCommandExecutor) getCommandExecutor();
+    FirefoxProfile profileToUse = getProfile(executor.profile);
+    profileToUse.addWebDriverExtensionIfNeeded(false);
+
+    // TODO(simon): Make this not sinfully ugly
+    ExtensionConnection connection = connectTo(executor.binary, profileToUse, "localhost");
+    executor.setConnection(connection);
+
+    try {
+      connection.start();
+    } catch (IOException e) {
+      throw new WebDriverException("An error occurred while connecting to Firefox", e);
+    }
+  }
+
+  private FirefoxProfile getProfile(FirefoxProfile profile) {
     FirefoxProfile profileToUse = profile;
     String suggestedProfile = System.getProperty("webdriver.firefox.profile");
     if (profileToUse == null && suggestedProfile != null) {
@@ -104,23 +128,28 @@ public class FirefoxDriver extends RemoteWebDriver implements TakesScreenshot, F
     } else if (profileToUse == null) {
       profileToUse = new FirefoxProfile();
     }
-    profileToUse.addWebDriverExtensionIfNeeded(false);
-
-    return ExtensionConnectionFactory.connectTo(binary, profileToUse, "localhost");
+    return profileToUse;
   }
 
-  @Override
-  protected void startClient() {
+  protected ExtensionConnection connectTo(FirefoxBinary binary, FirefoxProfile profile,
+                                          String host) {
+    int profilePort = profile.getPort() == 0 ? DEFAULT_PORT : profile.getPort();
+    Lock lock = new SocketLock(profilePort - 1);
     try {
-      ((ExtensionConnection) this.getCommandExecutor()).start();
-    } catch (IOException e) {
-      throw new WebDriverException("An error occurred while connecting to Firefox", e);
+      FirefoxBinary bin = binary == null ? new FirefoxBinary() : binary;
+      
+
+      return new NewProfileExtensionConnection(lock, bin, profile, host);
+    } catch (Exception e) {
+      throw new WebDriverException(e);
+    } finally {
+      lock.unlock();
     }
   }
 
   @Override
   protected void stopClient() {
-    ((ExtensionConnection) this.getCommandExecutor()).quit();
+    ((LazyCommandExecutor) this.getCommandExecutor()).quit();
   }
 
   @Override
@@ -129,95 +158,99 @@ public class FirefoxDriver extends RemoteWebDriver implements TakesScreenshot, F
   }
 
   public WebElement findElementByCssSelector(String using) {
-    if (using == null)
-     throw new IllegalArgumentException("Cannot find elements when the css selector is null.");
+    if (using == null) {
+      throw new IllegalArgumentException("Cannot find elements when the css selector is null.");
+    }
 
     return findElement("css selector", using);
   }
 
   public List<WebElement> findElementsByCssSelector(String using) {
-    if (using == null)
-     throw new IllegalArgumentException("Cannot find elements when the css selector is null.");
+    if (using == null) {
+      throw new IllegalArgumentException("Cannot find elements when the css selector is null.");
+    }
 
     return findElements("css selector", using);
   }
 
-    @Override
-    public TargetLocator switchTo() {
-        return new FirefoxTargetLocator();
+  @Override
+  public TargetLocator switchTo() {
+    return new FirefoxTargetLocator();
+  }
+
+  @Override
+  protected Response execute(DriverCommand driverCommand, Map<String, ?> parameters) {
+    if (currentAlert != null) {
+      if (!alertWhiteListedCommands.contains(driverCommand)) {
+        ((FirefoxTargetLocator) switchTo()).alert()
+            .dismiss();
+        throw new UnhandledAlertException(driverCommand.toString());
+      }
     }
 
-    @Override
-    protected Response execute(DriverCommand driverCommand, Map<String, ?> parameters) {
-      if (currentAlert != null) {
-        if (!alertWhiteListedCommands.contains(driverCommand)) {
-          ((FirefoxTargetLocator) switchTo()).alert().dismiss();
-          throw new UnhandledAlertException(driverCommand.toString());
-        }
+    Response response = super.execute(driverCommand, parameters);
+
+    Object rawResponse = response.getValue();
+    if (rawResponse instanceof Map) {
+      Map map = (Map) rawResponse;
+      if (map.containsKey("__webdriverType")) {
+        // Looks like have an alert. construct it
+        currentAlert = new FirefoxAlert((String) map.get("text"));
+        response.setValue(null);
       }
-
-      Response response = super.execute(driverCommand, parameters);
-
-      Object rawResponse = response.getValue();
-      if (rawResponse instanceof Map) {
-        Map map = (Map) rawResponse;
-        if (map.containsKey("__webdriverType")) {
-          // Looks like have an alert. construct it
-          currentAlert = new FirefoxAlert((String) map.get("text"));
-          response.setValue(null);
-        }
-      }
-
-      return response;
     }
+
+    return response;
+  }
 
   @Override
   public boolean isJavascriptEnabled() {
     return true;
   }
 
-    private class FirefoxTargetLocator extends RemoteTargetLocator {
-        // TODO: this needs to be on an interface
-        public Alert alert() {
-          if (currentAlert != null) {
-            return currentAlert;
-          }
+  private class FirefoxTargetLocator extends RemoteTargetLocator {
+    // TODO: this needs to be on an interface
 
-          throw new NoAlertPresentException();
-        }
-    }
-
-    public <X> X getScreenshotAs(OutputType<X> target) {
-      // Get the screenshot as base64.
-      String base64 = execute(DriverCommand.SCREENSHOT).getValue().toString();
-      // ... and convert it.
-      return target.convertFromBase64Png(base64);
-    }
-
-    /**
-     * Saves a screenshot of the current page into the given file.
-     *
-     * @deprecated Use getScreenshotAs(file), which returns a temporary file.
-     */
-    @Deprecated
-    public void saveScreenshot(File pngFile) {
-      if (pngFile == null) {
-          throw new IllegalArgumentException("Method parameter pngFile must not be null");
+    public Alert alert() {
+      if (currentAlert != null) {
+        return currentAlert;
       }
 
-      File tmpfile = getScreenshotAs(FILE);
-
-      File dir = pngFile.getParentFile();
-      if (dir != null && !dir.exists() && !dir.mkdirs()) {
-          throw new WebDriverException("Could not create directory " + dir.getAbsolutePath());
-      }
-
-      try {
-        FileHandler.copy(tmpfile, pngFile);
-      } catch (IOException e) {
-        throw new WebDriverException(e);
-      }
+      throw new NoAlertPresentException();
     }
+  }
+
+  public <X> X getScreenshotAs(OutputType<X> target) {
+    // Get the screenshot as base64.
+    String base64 = execute(DriverCommand.SCREENSHOT).getValue().toString();
+    // ... and convert it.
+    return target.convertFromBase64Png(base64);
+  }
+
+  /**
+   * Saves a screenshot of the current page into the given file.
+   *
+   * @deprecated Use getScreenshotAs(file), which returns a temporary file.
+   */
+  @Deprecated
+  public void saveScreenshot(File pngFile) {
+    if (pngFile == null) {
+      throw new IllegalArgumentException("Method parameter pngFile must not be null");
+    }
+
+    File tmpfile = getScreenshotAs(FILE);
+
+    File dir = pngFile.getParentFile();
+    if (dir != null && !dir.exists() && !dir.mkdirs()) {
+      throw new WebDriverException("Could not create directory " + dir.getAbsolutePath());
+    }
+
+    try {
+      FileHandler.copy(tmpfile, pngFile);
+    } catch (IOException e) {
+      throw new WebDriverException(e);
+    }
+  }
 
   private class FirefoxAlert implements Alert {
     private String text;
@@ -236,6 +269,29 @@ public class FirefoxDriver extends RemoteWebDriver implements TakesScreenshot, F
 
     public String getText() {
       return text;
+    }
+  }
+
+  private static class LazyCommandExecutor implements CommandExecutor {
+    private ExtensionConnection connection;
+    private final FirefoxBinary binary;
+    private final FirefoxProfile profile;
+
+    private LazyCommandExecutor(FirefoxBinary binary, FirefoxProfile profile) {
+      this.binary = binary;
+      this.profile = profile;
+    }
+
+    public void setConnection(ExtensionConnection connection) {
+      this.connection = connection;
+    }
+
+    public void quit() {
+      connection.quit();
+    }
+
+    public Response execute(Command command) throws Exception {
+      return connection.execute(command);
     }
   }
 }
