656c9d9edd3760aca70634b3cf47b4dedfb16bf6
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2010-05-08 11:45:15 +0000

    SimonStewart: Deleting the build grammar branch
    git-svn-id: http://selenium.googlecode.com/svn@8832 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/build_grammar/common/src/js/spoof_header.js
+++
@@ -1,9 +0,0 @@
-
-// Override some pieces that might reasonably be expected to exist, but don't 
-// when in an extension
-
-var window = window || {};
-
-var navigator = navigator || { userAgent: "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7 (.NET CLR 3.5.30729)" };
-
-var top = top || {};
