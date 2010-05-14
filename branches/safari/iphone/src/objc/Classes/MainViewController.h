ab4dd43f43edeb390d1685a4ff2abe287f7513cc
fazekasmiklos <fazekasmiklos@07704840-8298-11de-bf8c-fd130f914ac9>
2010-05-14 14:03:20 +0000

    FazekasMiklos: Warning fix WebViewController renamed to WebViewControllerIPhone
    git-svn-id: http://selenium.googlecode.com/svn@8923 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/safari/iphone/src/objc/Classes/MainViewController.h
+++ branches/safari/iphone/src/objc/Classes/MainViewController.h
@@ -17,7 +17,7 @@
 //  limitations under the License.
 
 #import <UIKit/UIKit.h>
-#import "WebViewController.h"
+#import "WebViewControllerIPhone.h"
 
 // |MainViewController| is the |UIViewController| responsible for the main
 // view. The view contains a |UIWebView and a |UILabel| for status display.
@@ -25,11 +25,11 @@
 // [MainViewController sharedInstance];
 // The UI element definitions are contained in MainView.xib.
 @interface MainViewController : UIViewController {
-  IBOutlet WebViewController *webViewController;
+  IBOutlet WebViewControllerIPhone *webViewController;
   IBOutlet UILabel *statusLabel_;
 }
 
-@property (nonatomic, retain) WebViewController *webViewController;
+@property (nonatomic, retain) WebViewControllerIPhone *webViewController;
 
 // Set the current status text.
 - (void)describeLastAction:(NSString *)status;
