2d6b9b14e0c44ed472d51f659a7ef01f744ae07d
eran.mes@gmail.com <eran.mes@gmail.com@07704840-8298-11de-bf8c-fd130f914ac9>
2010-07-23 15:35:35 +0000

    EranMes: Cleaning up copyright and author info.
    git-svn-id: http://selenium.googlecode.com/svn@9378 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/interactions_api/common/src/cpp/webdriver-interactions/translate_keycode_linux.h
+++ branches/interactions_api/common/src/cpp/webdriver-interactions/translate_keycode_linux.h
@@ -1,5 +1,21 @@
-// Copyright 2009 Google Inc. All Rights Reserved.
-// Author: eranm@google.com (Eran Messeri)
+/*
+Copyright 2007-2010 WebDriver committers
+Copyright 2007-2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Author: eran.mes@gmail.com (Eran Mes)
 //
 //
 #ifndef _TRANSLATE_KEYCODE_LINUX_H
