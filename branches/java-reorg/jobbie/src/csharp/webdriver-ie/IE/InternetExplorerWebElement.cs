d68e96c0957e6f20823341892ced4c0f2c729558
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2011-01-05 17:16:39 +0000

    SimonStewart: Setting up the java-reorg folder again
    git-svn-id: http://selenium.googlecode.com/svn@10850 07704840-8298-11de-bf8c-fd130f914ac9

---
+++ branches/java-reorg/jobbie/src/csharp/webdriver-ie/IE/InternetExplorerWebElement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Globalization;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Remote;
+
+namespace OpenQA.Selenium.IE
+{
+    /// <summary>
+    /// InternetExplorerWebElement allows you to have access to specific items that are found on the page.
+    /// </summary>
+    /// <seealso cref="IRenderedWebElement"/>
+    /// <seealso cref="ILocatable"/>
+    /// <example>
+    /// <code>
+    /// [Test]
+    /// public void TestGoogle()
+    /// {
+    ///     driver = new InternetExplorerDriver();
+    ///     InternetExplorerWebElement elem = driver.FindElement(By.Name("q"));
+    ///     elem.SendKeys("Cheese please!");
+    /// }
+    /// </code>
+    /// </example>
+    public class InternetExplorerWebElement : RenderedRemoteWebElement, ILocatable
+    {
+        /// <summary>
+        /// Initializes a new instance of the InternetExplorerWebElement class.
+        /// </summary>
+        /// <param name="parent">Driver in use.</param>
+        /// <param name="id">ID of the element.</param>
+        public InternetExplorerWebElement(InternetExplorerDriver parent, string id)
+            : base(parent, id)
+        {
+        }
+
+        #region ILocatable Members
+        /// <summary>
+        /// Gets the point of the element once scrolling has completed
+        /// </summary>
+        public Point LocationOnScreenOnceScrolledIntoView
+        {
+            get
+            {
+                Dictionary<string, object> parameters = new Dictionary<string, object>();
+                parameters.Add("id", Id);
+                Response commandResponse = Execute(DriverCommand.GetElementLocationOnceScrolledIntoView, parameters);
+                Dictionary<string, object> rawPoint = (Dictionary<string, object>)commandResponse.Value;
+                int x = Convert.ToInt32(rawPoint["x"], CultureInfo.InvariantCulture);
+                int y = Convert.ToInt32(rawPoint["y"], CultureInfo.InvariantCulture);
+                return new Point(x, y);
+            }
+        }
+        #endregion
+    }
+}
