fa648d2aaf7c7f3f8a69da440811a092a27543ec
simon.m.stewart <simon.m.stewart@07704840-8298-11de-bf8c-fd130f914ac9>
2011-02-27 19:45:53 +0000

    SimonStewart: The junit 4 migration branch is now antiquated. Deleting it, so that we can bring it up to date more easily
    git-svn-id: http://selenium.googlecode.com/svn@11551 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/junit4/iphone/test/java/org/openqa/selenium/iphone/MultitouchTest.java
+++
@@ -1,5 +0,0 @@
-package org.openqa.selenium.iphone;
-
-public class MultitouchTest {
-
-}
--- branches/junit4/selenium/src/csharp/WebdriverBackedSelenium/Internal/SeleniumEmulation/ILookupStrategy.cs
+++
@@ -1,9 +0,0 @@
-using OpenQA.Selenium;
-
-namespace Selenium
-{
-	public interface ILookupStrategy
-	{
-		IWebElement Find(IWebDriver driver, string use);
-	}
-}
