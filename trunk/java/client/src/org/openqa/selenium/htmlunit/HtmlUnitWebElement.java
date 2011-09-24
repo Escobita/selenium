Escobita/selenium

commit 616514e3b18b8fddc58995cea62d863116b821cd

AlexeiBarantsev: isSelected should return true for the first option (by default) if none is selected. Fixes issue 2295
git-svn-id: http://selenium.googlecode.com/svn@13942 07704840-8298-11de-bf8c-fd130f914ac9

trunk/java/client/src/org/openqa/selenium/htmlunit/HtmlUnitWebElement.java
@@ -431,7 +431,7 @@ public class HtmlUnitWebElement implements WrapsDriver,
     if (element instanceof HtmlInput) {
       return ((HtmlInput) element).isChecked();
     } else if (element instanceof HtmlOption) {
-      return ((HtmlOption) element).hasAttribute("selected");
+      return ((HtmlOption) element).isSelected();
     }
 
     throw new UnsupportedOperationException(

trunk/common/src/web/formPage.html
@@ -58,6 +58,13 @@ There should be a form here:
       <option>Oranges</option>
     </select>
 
+    <select name="select-default">
+        <option>One</option>
+        <option>Two</option>
+        <option>Four</option>
+        <option>Still learning how to count, apparently</option>
+    </select>
+
     <br/>
 
     <input type="radio" id="cheese" name="snack" value="cheese"/>Cheese<br/>

trunk/java/client/test/org/openqa/selenium/SelectElementHandlingTest.java
@@ -82,4 +82,20 @@ public class SelectElementHandlingTest extends AbstractDriverTestCase {
           is(true));
     }
   }
+
+  @Ignore({SELENESE, ANDROID})
+  public void testShouldSelectFirstOptionByDefaultIfNoneIsSelected() {
+    driver.get(pages.formPage);
+    WebElement selectBox = driver.findElement(By.xpath("//select[@name='select-default']"));
+    List<WebElement> options = selectBox.findElements(By.tagName("option"));
+    WebElement one = options.get(0);
+    WebElement two = options.get(1);
+    assertThat(one.isSelected(), is(true));
+    assertThat(two.isSelected(), is(false));
+
+    two.click();
+    assertThat(one.isSelected(), is(false));
+    assertThat(two.isSelected(), is(true));
+  }
+
 }

commit 32362e5b8d3d46b152c6cbd902173d25936dcb89

AlexeiBarantsev: A better implementation of handling click on an HtmlOption element. It passes all tests that were broken by the previous commit r13942. See also issue 1899.
git-svn-id: http://selenium.googlecode.com/svn@13943 07704840-8298-11de-bf8c-fd130f914ac9

trunk/java/client/src/org/openqa/selenium/htmlunit/HtmlUnitWebElement.java
@@ -108,14 +108,23 @@ public class HtmlUnitWebElement implements WrapsDriver,
       // element not visible either
     }
 
-    // TODO(simon): It appears as if clicking on html options doesn't toggle state
     if (element instanceof HtmlOption) {
-      boolean currentlySelected = isSelected();
-      ((HtmlOption) element).setSelected(!currentlySelected);
-      if (currentlySelected) {
-        element.removeAttribute("selected");
-      } else {
-        element.setAttribute("selected", "true");
+      HtmlOption option = (HtmlOption) element;
+      HtmlSelect select = option.getEnclosingSelect();
+      if (select.isMultipleSelectEnabled()) {
+          if (option.isSelected()) {
+            // HtmlUnit cannot unselect options of a multiselect list.
+            // We emulate 'unselecting', but we should not call mouse.click after that,
+            // if we do click the option will go back to selected.
+            // May be we should do something else, fire some events, etc.
+            // But at least this implementation passes all the current tests.
+            option.setSelected(false);
+            element.removeAttribute("selected");
+            return;
+            // yes, return, don't do mouse.click!
+          } else {
+            element.setAttribute("selected", "true");
+          }
       }
       // Now fall through
     }