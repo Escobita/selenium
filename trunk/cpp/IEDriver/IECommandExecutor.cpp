Escobita/selenium

commit 5bc816d32e546639650601bee73597ae907f6081

AlexeiBarantsev: Handling unhandled alerts automatically, step 2 - IE
git-svn-id: http://selenium.googlecode.com/svn@15643 07704840-8298-11de-bf8c-fd130f914ac9

trunk/cpp/IEDriver/IECommandExecutor.cpp
@@ -320,11 +320,27 @@ void IECommandExecutor::DispatchCommand() {
   if (found_iterator == this->command_handlers_.end()) {
     response.SetErrorResponse(501, "Command not implemented");
   } else {
-    CommandHandlerHandle command_handler = found_iterator->second;
-    command_handler->Execute(*this, this->current_command_, &response);
-
     BrowserHandle browser;
     int status_code = this->GetCurrentBrowser(&browser);
+    if (status_code == SUCCESS) {
+      HWND alert_handle = browser->GetActiveDialogWindowHandle();
+      int command_type = this->current_command_.command_type();
+      if (alert_handle != NULL &&
+          command_type != GetAlertText &&
+          command_type != SendKeysToAlert &&
+          command_type != AcceptAlert &&
+          command_type != DismissAlert) {
+        response.SetErrorResponse(EMODALDIALOGOPENED, "Modal dialog present");
+        ::SendMessage(alert_handle, WM_COMMAND, IDCANCEL, NULL);
+        this->serialized_response_ = response.Serialize();
+        return;
+      }
+    }
+
+	CommandHandlerHandle command_handler = found_iterator->second;
+    command_handler->Execute(*this, this->current_command_, &response);
+
+    status_code = this->GetCurrentBrowser(&browser);
     if (status_code == SUCCESS) {
       this->is_waiting_ = browser->wait_required();
       if (this->is_waiting_) {

trunk/java/client/test/org/openqa/selenium/AlertsTest.java
@@ -57,19 +57,6 @@ public class AlertsTest extends AbstractDriverTestCase {
   }
 
   @JavascriptEnabled
-  @Ignore(ALL)
-  public void testShouldDismissAlertOnException() {
-    driver.findElement(By.id("alert")).click();
-
-    try {
-      driver.getTitle();
-      fail("Expected exception");
-    } catch (NoAlertPresentException expected) {
-    }
-    assertEquals("Testing Alerts", driver.getTitle());
-  }
-
-  @JavascriptEnabled
   public void testShouldAllowUsersToAcceptAnAlertManually() {
     driver.findElement(By.id("alert")).click();
 
@@ -203,8 +190,8 @@ public class AlertsTest extends AbstractDriverTestCase {
     assertEquals("Testing Alerts", driver.getTitle());
   }
 
-  @Ignore({ANDROID, CHROME, HTMLUNIT, IE})
-  public void testShouldThrowAnExceptionIfAnAlertHasNotBeenDealtWith() {
+  @Ignore({ANDROID, CHROME, HTMLUNIT})
+  public void testShouldThrowAnExceptionIfAnAlertHasNotBeenDealtWithAndDismissTheAlert() {
     driver.findElement(By.id("alert")).click();
     try {
       driver.getTitle();

commit 2d11585056f4bd241f7acb9607ba4841433db1e1

JimEvans: Correcting the error introduced in r15643 that throws an exception when an unhandled alert is present. Modal dialogs created by showModalDialog() aren't alerts; they're windows in their own right.
git-svn-id: http://selenium.googlecode.com/svn@15807 07704840-8298-11de-bf8c-fd130f914ac9

trunk/cpp/IEDriver/IECommandExecutor.cpp
@@ -323,9 +323,19 @@ void IECommandExecutor::DispatchCommand() {
     BrowserHandle browser;
     int status_code = this->GetCurrentBrowser(&browser);
     if (status_code == SUCCESS) {
+      bool alert_is_active = false;
       HWND alert_handle = browser->GetActiveDialogWindowHandle();
+      if (alert_handle != NULL) {
+        // Found a window handle, make sure it's an actual alert,
+        // and not a showModalDialog() window.
+        vector<char> window_class_name(34);
+        ::GetClassNameA(alert_handle, &window_class_name[0], 34);
+        if (strcmp("#32770", &window_class_name[0]) == 0) {
+          alert_is_active = true;
+        }
+      }
       int command_type = this->current_command_.command_type();
-      if (alert_handle != NULL &&
+      if (alert_is_active &&
           command_type != GetAlertText &&
           command_type != SendKeysToAlert &&
           command_type != AcceptAlert &&