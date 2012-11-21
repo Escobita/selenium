05a1303654178d6a4a38c3d37aa8eb46ce17fc4a
dawagner <dawagner@07704840-8298-11de-bf8c-fd130f914ac9>
2012-11-21 15:24:36 +0000

    DanielWagnerHall: Removing dead branch
    git-svn-id: http://selenium.googlecode.com/svn@18181 07704840-8298-11de-bf8c-fd130f914ac9

--- branches/fennecdriver/java/client/test/org/openqa/selenium/internal/IgnoredTestCallback.java
+++
@@ -1,7 +0,0 @@
-package org.openqa.selenium.internal;
-
-import org.openqa.selenium.Ignore;
-
-public interface IgnoredTestCallback {
-  void callback(Class className, String testName, Ignore ignore);
-}
